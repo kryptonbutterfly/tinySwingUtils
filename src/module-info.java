8298fb312f7943edfd3924ee08ce9b61ad74fe15
tinycodecrank <tinycodecrank@gmail.com>
2022-06-13 20:06:38 +0200

    update to jre 18

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module de.tinycodecrank.SwingUtils
+{
+	exports de.tinycodecrank.util.swing.events;
+	exports de.tinycodecrank.util.swing.model;
+	exports de.tinycodecrank.util.swing;
+	
+	requires Monads;
+	requires transitive java.desktop;
+	requires mathUtils;
+}
